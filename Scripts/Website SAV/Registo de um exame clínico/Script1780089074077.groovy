5f75a63bfe868bac83c120e88a03a29687955178
adrianabmartins <adrianabarreiromartins@ua.pt>
2026-05-29 23:57:28 +0100

    Add files via upload

---
+++ Scripts/Website SAV/Registo de um exame clínico/Script1780089074077.groovy
@@ -0,0 +1,58 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.testobject.SelectorMethod
+
+import com.thoughtworks.selenium.Selenium
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.WebDriver
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium
+import static org.junit.Assert.*
+import java.util.regex.Pattern
+import static org.apache.commons.lang3.StringUtils.join
+import org.testng.asserts.SoftAssert
+import com.kms.katalon.core.testdata.CSVData
+import org.openqa.selenium.Keys as Keys
+
+SoftAssert softAssertion = new SoftAssert();
+WebUI.openBrowser('https://www.google.com/')
+def driver = DriverFactory.getWebDriver()
+String baseUrl = "https://www.google.com/"
+selenium = new WebDriverBackedSelenium(driver, baseUrl)
+selenium.open("https://kamylu.github.io/Plataforma_Web_SAV/")
+selenium.click("id=btn-choose-vet")
+selenium.type("id=login-user", "cristina")
+selenium.click("id=login-pass")
+selenium.type("id=login-pass", "1234")
+selenium.click("id=btn-submit-login")
+selenium.click("id=btn-read-chip")
+selenium.click("id=btn-validate-sav")
+selenium.click("id=btn-init-ato")
+selenium.click("xpath=//div[@id='ato-form-wrapper']/div/button[3]")
+selenium.click("id=ato-exame")
+selenium.type("id=ato-exame", "Hemograma")
+selenium.click("id=ato-obs")
+selenium.type("id=ato-obs", "resumo")
+selenium.click("id=ato-file")
+selenium.click("id=ato-file")
+selenium.click("id=btn-save-ato")
+selenium.click("xpath=//li[@onclick='openReportModal(1780083789545)']")
+selenium.click("id=btn-hide-modal")
---
+++ Test Suites/Website SAV.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Website SAV</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>971350d7-792c-4390-a0ff-d81831d7a4c1</testSuiteGuid>
+   <testCaseLink>
+      <guid>6ef1523a-881b-491a-baca-5e845c899053</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Registo de uma consulta clínica</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>2c8f0b8d-c26f-41d4-b923-aa0ba7233bb1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Registo de uma vacinação</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>05468c32-917d-47c5-886f-bc905884cb33</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Registo de um exame clínico</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>2787c965-3d70-4e62-8352-fb4ecddd8684</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Registo de uma cirurgia</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>553c40ca-836c-4170-b41d-0cf94acbc273</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Consultar a identificação do animal com microchip</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>f4fed4c3-36d7-4f1c-af16-5d6701d97bfc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Consultar a identificação do animal sem microchip</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>f73f188c-1682-4361-9dce-70852a44e215</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Atualizar a informação do animal</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>e4ab4540-1d0d-46ba-abb8-4379e5c4fbc2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Efetuar login</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>4b4fe4b2-f53f-4bef-8b32-045956c42e60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Consultar Histórico Universal do Animal Consulta automática com sucesso</testCaseId>
+   </testCaseLink><testCaseLink>
+      <guid>63cf40bd-b8a4-42b6-ad2d-ed77cb711ed1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Website SAV/Falha na comunicação com a SIAC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
+    
